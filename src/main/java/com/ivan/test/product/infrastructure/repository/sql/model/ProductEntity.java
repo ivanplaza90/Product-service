6489b7cd6c0c443aa51439340eb32b6d46e1c9e7
Ivan Plaza <100290861@alumnos.uc3m.es>
2023-04-22 19:00:22 +0200

    Add product sql repository implemtation

--- src/main/java/com/ivan/test/product/infrastructure/repository/sql/model/ProductEntity.java
+++ src/main/java/com/ivan/test/product/infrastructure/repository/sql/model/ProductEntity.java
@@ -1,23 +1,30 @@
-package com.ivan.test.product.domain.model;
+package com.ivan.test.product.infrastructure.repository.sql.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
 import java.time.Instant;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class ProductEntity {
 
+    @Id
+    private int id;
     private int productId;
     private int brandId;
     private Instant startDate;
     private Instant endDate;
     private int priceList;
     private int priority;
-    private Money price;
+    private BigDecimal price;
+    private String currency;
 }
